4b15a0a752caa32148f18eaed3172556814693c7
Graeme Armstrong <garmstrong@lcs.on.ca>
2022-11-09 09:30:35 -0500

    Created reset button

--- StopwatchApp/ContentView.swift
+++ StopwatchApp/ContentView.swift
@@ -22,6 +22,32 @@ struct ContentView: View {
                     .font(Font.system(size: 90, weight: .thin))
                     .foregroundColor(.white)
                 
+                // Create a circular button
+                ZStack {
+                    
+                    // First layer
+                    Circle()
+                        .foregroundColor(.gray)
+                        .frame(width: 100)
+                    
+                    // Second layer
+                    Circle()
+                        .foregroundColor(.black)
+                        .frame(width: 93)
+                
+                    // Third layer
+                    Circle()
+                        .foregroundColor(.gray)
+                        .frame(width: 89)
+                    
+                    // Forth layer (text)
+                    Text("Reset")
+                        .foregroundColor(.white)
+                        .font(.title2)
+                    
+                    
+                    
+                }
             }
             .padding()
         }
